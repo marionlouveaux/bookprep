0e838e66d8d0d56df2b7747a9a1e9bf2ffae01ae
marionlouveaux <marion.louveaux@gmail.com>
2021-05-25 14:28:34 +0200

    Add test to find_and_replace()
    - Test error message (function stops and outputs an error message if file path is wrong)

--- tests/testthat/test-find_and_replace.R
+++ tests/testthat/test-find_and_replace.R
@@ -19,3 +19,20 @@ test_that("find_and_replace works", {
   expect_equal(index_lines[2], 'title: \"My book\"')
   expect_equal(index_lines[3], 'author: "Marion Louveaux"')
 })
+
+
+test_that("find_and_replace gives error", {
+  skip_if_translated()
+  expect_error(
+    object = find_and_replace(
+      file = "non_existing_path",
+      replacements = c(
+        "book_title" = "My book",
+        "author_name" = "Marion Louveaux",
+        "creation_date" = "`r Sys.Date()`",
+        "short_description" = "An example of book with {bookprep}.",
+        "index_title" = "Context"
+      )
+    ), "The specified file does not exist."
+  )
+})
